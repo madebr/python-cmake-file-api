81f572a2bff733af4a1e88df3f5bd1ecc031be89
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-04-27 22:21:32 +0200

    Don't pass text= attribute to subprocess.check_output

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -13,7 +13,7 @@ from cmake_file_api.kinds.codemodel.api import CODEMODEL_API
 
 @functools.lru_cache(1)  # FIXME: CPython 3.9 provides `functools.cache`
 def cmake_version():
-    cmake_version_raw = subprocess.check_output(["cmake", "--version"], text=True)
+    cmake_version_raw = subprocess.check_output(["cmake", "--version"]).decode()
     cmake_version_match = next(re.finditer(r"cmake version ((?:[0-9.]+.)[0-9.]+)", cmake_version_raw, flags=re.I))
     version_list = cmake_version_match.group(1).split(".")
     version_tuple = tuple(int(v) for v in version_list)
