5d16e5c684932ac5640379acf20a2f4d79fc5011
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-04-03 18:47:47 +0200

    Move BacktraceGraph and BacktraceNode to common file

--- cmake_file_api/kinds/codemodel/target/v2.py
+++ cmake_file_api/kinds/codemodel/target/v2.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from typing import Dict, List, Optional
 
 from cmake_file_api.kinds.common import CMakeSourceBuildPaths
+from cmake_file_api.kinds.common.backtraceGraph import BacktraceNode, BacktraceGraph
 
 
 class TargetType(enum.Enum):
@@ -25,59 +26,6 @@ class LinkFragmentRole(enum.Enum):
 
 class ArchiveFragmentRole(enum.Enum):
     ARCHIVER_FLAGS = "flags"
-
-
-class BacktraceNode(object):
-    __slots__ = ("file", "line", "command", "parent")
-
-    def __init__(self, file: Path, line: Optional[int], command: Optional[str]):
-        self.file = file
-        self.line = line
-        self.command = command
-        self.parent = None
-
-    @classmethod
-    def from_dict(cls, dikt: Dict, commands: List[str], files: List[Path]) -> "BacktraceNode":
-        file = files[dikt["file"]]
-        line = dikt.get("line")
-        command = None
-        if "command" in dikt:
-            command = commands[dikt["command"]]
-        return cls(file, line, command)
-
-    def update_from_dict(self, dikt: Dict, nodes: List["BacktraceNode"]) -> None:
-        if "parent" in dikt:
-            self.parent = nodes[dikt["parent"]]
-
-    def __repr__(self) -> str:
-        return "{}(file='{}', line={}, command={}".format(
-            type(self).__name__,
-            self.file,
-            self.line,
-            self.command,
-        )
-
-
-class BacktraceGraph(object):
-    __slots__ = ("nodes", )
-
-    def __init__(self, nodes: List[BacktraceNode]):
-        self.nodes = nodes
-
-    @classmethod
-    def from_dict(cls, dikt: Dict) -> "BacktraceGraph":
-        commands = dikt["commands"]
-        files = list(Path(f) for f in dikt["files"])
-        nodes = list(BacktraceNode.from_dict(btn, commands, files) for btn in dikt["nodes"])
-        for node, dikt_node in zip(nodes, dikt["nodes"]):
-            node.update_from_dict(dikt_node, nodes)
-        return cls(nodes)
-
-    def __repr__(self) -> str:
-        return "{}(nodes={})".format(
-            type(self).__name__,
-            self.nodes,
-        )
 
 
 class TargetDestination(object):
--- cmake_file_api/kinds/kind.py
+++ cmake_file_api/kinds/kind.py
@@ -4,6 +4,7 @@ import enum
 class ObjectKind(enum.Enum):
     CACHE = "cache"
     CMAKEFILES = "cmakeFiles"
+    CTESTINFO = "ctestInfo"
     CODEMODEL = "codemodel"
     CONFIGURELOG = "configureLog"
     TOOLCHAINS = "toolchains"
